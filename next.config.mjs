e22422dda03b7f158400b605f1b4e310e95be3ca
Leonardo Ferreira Lima <80768424@redecorp.br>
2023-04-14 09:02:08 -0300

    chore: add pt locale for i18n on next config

--- next.config.mjs
+++ next.config.mjs
@@ -15,8 +15,9 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
+    locales: ["en", "pt"],
     defaultLocale: "en",
   },
 };
+
 export default config;
